ed0eebe60cd76697633ec25369a78eb23d8207cc
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-06-14 15:17:31 +0530

    profiles route added

---
+++ pages/profiles/[id].js
@@ -0,0 +1,69 @@
+import Head from "next/head";
+import { useEffect } from "react";
+import { API_URL } from "@/config/index";
+
+const Profile = ({ title, ogTitle, ogImage, ogDescription, id, city }) => {
+  useEffect(() => console.log(id));
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:url" content={ogImage} />
+        <meta
+          property="og:image"
+          content={`https://wouldbee.vercel.app/profiles/${id}`}
+        />
+        <meta property="og:type" content="profile" />
+        <meta property="og:description" content={ogDescription} />
+      </Head>
+      <h1>{id}</h1>
+      <p>{city}</p>
+    </>
+  );
+};
+
+Profile.defaultProps = {
+  id: "",
+  title:
+    "Would Bee - Free Matrimony & Match Making, Jeevansathi Search for Shaadi in Bharat",
+  ogTitle: "Open Graph Meta Tags: Everything You Need to Know",
+
+  ogImage:
+    "https://ahrefs.com/blog/wp-content/uploads/2020/01/fb-open-graph-1.jpg",
+  ogDescription:
+    "Learn about 13 features that set Ahrefs apart from the competition.",
+};
+
+export default Profile;
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/profiles/${id}`);
+
+//   const data = await res.json();
+
+//   const paths = data.map((d) => ({
+//     params: { id: d.id },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+export async function getServerSideProps({ params: { id } }) {
+  const res = await fetch(`${API_URL}/profiles/${id}`);
+
+  const data = await res.json();
+  console.log(data);
+
+  return {
+    props: {
+      id: data.id,
+      city: data.city,
+    },
+  };
+}
