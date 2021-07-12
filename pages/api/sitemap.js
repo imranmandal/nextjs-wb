11e17e85e06c8fee973698ee75445bd18ae081ea
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-07-12 15:05:51 +0530

    sitemap updated

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -10,6 +10,14 @@ export default async (req, res) => {
     { url: "/FAQ", changefreq: "never", priority: 0.3 },
     { url: "/privacy", changefreq: "never", priority: 0.3 },
     { url: "/terms", changefreq: "never", priority: 0.3 },
+    { url: "/wb/landing-page", changefreq: "never", priority: 0.3 },
+    { url: "/wb/baniya", changefreq: "never", priority: 0.3 },
+    { url: "/wb/jain", changefreq: "never", priority: 0.3 },
+    { url: "/wb/muslim", changefreq: "never", priority: 0.3 },
+    { url: "/wb/punjabi", changefreq: "never", priority: 0.3 },
+    { url: "/wb/christian", changefreq: "never", priority: 0.3 },
+    { url: "/wb/marathi", changefreq: "never", priority: 0.3 },
+    { url: "/wb/south", changefreq: "never", priority: 0.3 },
   ];
 
   // Create a stream to write to
