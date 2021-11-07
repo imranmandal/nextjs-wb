17b14a456fcfa53c25b0c819ed5b6a097e3890c2
imranmandal <64127903+imranmandal@users.noreply.github.com>
2021-11-07 10:51:36 +0530

    comments added in /config/index.js

--- config/index.js
+++ config/index.js
@@ -1,12 +1,17 @@
 // export const API_URL = "https://b9d55dc60881.in.ngrok.io";
+
+// Server URL
 export const API_URL =
   process.env.NEXT_PUBLIC_API_URL || "https://1277-110-235-219-249.in.ngrok.io";
 
+// Landing page URL
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_LANDING_PAGE_URL || "http://localhost:3000";
 
+// Web App URL
 export const WEB_APP_URL =
   process.env.WEB_APP_PUBLIC_URL || "https://wouldbee-web-app.netlify.app";
 
+// TAG Manager ID
 export const TAG_MANAGER_ID =
   process.env.NEXT_PUBLIC_TAG_MANAGER_ID || "GTM-KBJTVKT";
